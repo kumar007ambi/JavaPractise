d4d86de71ddd7376d9d65bd3429fb718df077663
AMBIKA KUMAR KEWAT <60788663+kumar007ambi@users.noreply.github.com>
2022-01-28 21:10:37 +0530

    2nd day

---
+++ PractiseSet3.java
@@ -0,0 +1,27 @@
+public class PractiseSet3 {
+    public static void main(String[] args) {
+        //Q1.Write a Java program to convert a string to lowercase.
+        //    String str = "HERO";
+        //    System.out.println(str.toLowerCase());
+
+        //Q2.Write a Java program to replace spaces with underscores.
+        //    String str = "Hello World";
+        //    System.out.println(str.replace(" ","_"));
+        
+        // Q3.Write a Java program to fill in a letter template which looks like below:
+        //    String letter = "Dear name, Thanks a lot";
+        //    letter = letter.replace("name", "Sachin");
+        //    System.out.println(letter);
+
+        // Q4.Write a Java program to detect double and triple spaces in a string.
+        // String myString = "This string contains double and  triple spaces";
+        // System.out.println(myString.indexOf("  "));
+        // System.out.println(myString.indexOf("   "));
+
+        // Q5.Write a program to format the following letter using escape sequence characters
+        String myLetter = "Dear Sachin,\n\tThis Java Course is Nice.\nThanks!";
+        System.out.println(myLetter);
+
+    }
+    
+}
